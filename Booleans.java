ff546274805b4273105ac71b06b2872a4f6ed5be
smithb <smithb@wlwv.k12.or.us>
2022-11-01 22:41:13 -0700

    initial commit of base code

---
+++ Booleans.java
@@ -0,0 +1,29 @@
+public class Booleans {
+    public static void main(String[] args) {
+        //our comparison operators are
+        //greater than >
+        //less than <
+        //equal to ==
+        //not equal to !=
+        //greater than or equal to >=
+        //less than or equal to <=
+
+        int month = 11;
+        int december = 12;
+        System.out.println(month < 11);
+        System.out.println(month <= 11);
+        System.out.println(month == december);
+        System.out.println(month != december);
+
+        //you don't have to use an if statement yet, you can just print the boolean value
+
+        int age1 = 12;
+        System.out.println("This person is an adult over 18: "); //complete this statement to print true or false
+        System.out.println("This person can drive a car legally: "); //complete this statement to print true or false
+
+        int age2 = 27;
+        System.out.println("This person is an adult over 18: "); //complete this statement to print true or false
+        System.out.println("This person can drive a car legally: "); //complete this statement to print true or false
+
+    }
+}
